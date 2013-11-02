f58224b63eb97543a7ddcda8187de81f93cbe65a
Joel Simon <joelsimon6@gmail.com>
2013-11-02 17:15:58 -0400

    added message

--- openSource1.js
+++ openSource1.js
@@ -1,4 +1,25 @@
-// Simple regex matcher for symbols * or c or .
+/*
+    Copyright (C) 2013 Joel Simon
+    Simple regex matcher for symbols * or c or .
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+*/
 'use strict';
 
 function doesMatch(pattern, word) {
@@ -10,12 +31,11 @@ function doesMatch(pattern, word) {
     if (pattern.length == 0 && word.length == 0) return true;
     if (pattern.length == 0 || word.length == 0) return false;
 
-    // I'm too lazy to do any modification of this code, so meh :-P
     // . case
-    if (p === '*') {
+    if (p === '.') {
         return doesMatch(psufix, wsufix);
     // * case
-    } else if (p == '.') {
+    } else if (p == '*') {
         if (pattern.length == 1) return true;
         var pnext = pattern.charAt(1);
         if (c === pnext || pnext === '.' || pnext === '*') {
@@ -35,19 +55,16 @@ function doesMatch(pattern, word) {
 }
 
 function testDoesMatch() {
-    assert (doesMatch('ab*cd.t', 'abksdhfkjdsfcdcdcdgt'), 'ab*cd.t');
-    assert (doesMatch('*.*', 'lskjfhldsjhflsa'), '*.*');
-    assert (!doesMatch('.', 'lskjfhldsjhflsa'), '.');
-    assert (doesMatch('.**', 'lskjfhldsjhflsa'), '.**');
-    assert (doesMatch('ab.*cd***d', 'abskjfhcdlcdsjddd'), 'ab.*cd***d');
-    assert (doesMatch('.*.*.', 'abc'), '.*.*.');
-    console.log("All tests passed!");
+  if (testr()) console.log("All tests passed!"); //yaaaa you rock :)
+  else console.log("You Failed!"); // :'(
+  function testr () {
+    if (!doesMatch('ab*cd.t', 'abksdhfkjdsfcdcdcdgt')) return false;
+    if (!doesMatch('*.*', 'lskjfhldsjhflsa')) return false;
+    if (doesMatch('.', 'lskjfhldsjhflsa')) return false;
+    if (!doesMatch('.**', 'lskjfhldsjhflsa')) return false;
+    if (!doesMatch('ab.*cd***d', 'abskjfhcdlcdsjddd')) return false;
+    if (!doesMatch('.*.*.', 'abc')) return false;
+    return true;
+  }
 }
-
-function assert(condition, message) {
-    if (!condition) {
-        throw message || "Assertion failed";
-    }
-}
-
 testDoesMatch();
